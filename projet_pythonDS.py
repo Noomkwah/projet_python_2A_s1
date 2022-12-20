1a9611364f63caf497b6e1bc92874d01acec4e1d
PaulD10 <104212237+PaulD10@users.noreply.github.com>
2022-12-20 15:44:36 +0100

    Extraction des données - Carte interactive
    Extraction des données à partir de l'API de la CE et création d'une carte intéractive

--- projet_pythonDS.py
+++ projet_pythonDS.py
@@ -1,80 +1,102 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
 import pandas as pd
-import plotly.express as px
-import plotly.io as pio
-import plotly.graph_objects as go
-pio.renderers.default = "browser"
-import plotly.graph_objects as go
 import urllib.request
 import urllib.error
-###------------------Scrapping----------------------
-#Le but est de créer une base de données avec toutes les infos nécessaires sur une zone particulière
-#ça se décompose en plusieurs étapes
-#1) On identifie la zone (latitude et longitude) 
-#2) On crée une base de données avec chacun des points dans lesquels on va récolter les données à partir du site
-#3) On fait la récolte de données
 
-#1 + 2) identification de la zone
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default = "browser"
 
-def zone(latitude_1,latitude_2,longitude_1, longitude_2, precision,startyear,endyear):
-    """On entre les latitudes et la longitudes qui permettent de définir la zone dans laquelle on récoltera les données
-        On renvoie un df avec chacun des points considérés"""
+###------------------Scrapping--------------------------------------------------------
 
-    #time= "20150105:1110"
+def extraction(latitude,longitude, startyear, endyear):
+    """Extraction de la base de données pour les paramètres de longitudes, latitudes et de périodes
+    à partir de l'api de la comission européenne https://re.jrc.ec.europa.eu"""
     
-    ls = []
-    temp = pd.DataFrame(columns = ["latitude","longitude","G(i)","WS10m"])
-    for longitude in range(longitude_1, (longitude_2+1), 1):
-        for latitude in range(latitude_1*precision,(latitude_2+1)*precision,1):
-        
+    req = "https://re.jrc.ec.europa.eu/api/seriescalc?lat="+str(latitude)+"&lon="+str(longitude)+"&startyear="+str(startyear)+"&endyear="+str(endyear)+"&month=0&showtemperatures=1&outputformat=csv&browser=1"
+    url = urllib.request.urlopen(req) 
+    return url
+
+def normalisation(url, latitude,longitude):
+    """On standardise les données récupérées pour les rendre comparables entre elles"""
+    
+    df_norm = pd.read_csv(url,skiprows=(8),skipfooter=9,engine='python') #On se débarasse des informations superflues
+    
+    df_norm['annee'] = df_norm['time'].str[:4] #On extrait la variable d'année à partir de la variable temps
+    df_norm = df_norm.groupby(['annee'])['G(i)', 'WS10m'].mean() #On calcule la moyenne annuelle de la variable ensoleillement et de la variable vitesse du vent
+    df_norm['latitude'] = latitude #On rajoute les paramètres de latitude et de longitude
+    df_norm["longitude"] = longitude
+
+    return df_norm
+
+
+def creation_df_zone(latitude_1,latitude_2,longitude_1, longitude_2, precision,startyear,endyear):
+    """Cette fonction sert à créer une base de données des moyennes d'ensoleillement et de vitesses du vent par an
+        sur une zone géographique définie par ses paramètres de latitude et de longitude. 
+        La variables précision donne la precision du quadrillage réalisé, type = int, > 0.
+        Les variables startyear et endyear permettent de définir l'intervalle de temps considéré (2005-2016).
+        Exemple : zone(1,2,-1,1,2,2010,2012)"""
+
+    ls_df = [] #création d'une liste vide qui servira a réunir les bases de données années par années
+    for longitude in range(longitude_1, (longitude_2+1), 1): #On parcourt un à un les longitude comprises entre longitude_1 et longitude_2
+        for latitude in range(latitude_1*precision,(latitude_2+1)*precision,1): #On parcourt les latitudes selon le meme principe en affinant le  quadrillage à l'aide de la variable précision
             latitude = latitude/precision 
             
-            try :  
+            try :  #try permet de ne pas considérer les coordonnées manquantes (ex : océan). 
+                url = extraction(latitude, longitude, startyear, endyear)#Récupération de la base de données brute
+                df_norm = normalisation(url, latitude, longitude)#Standardisation de la base de données
+                ls_df.append(df_norm)#Ajout de la base de données à la liste 
                 
-                req = "https://re.jrc.ec.europa.eu/api/seriescalc?lat="+str(latitude)+"&lon="+str(longitude)+"&startyear="+str(startyear)+"&endyear="+str(endyear)+"&month=0&showtemperatures=1&outputformat=csv&browser=1"
-                url = urllib.request.urlopen(req) 
-                pomme = pd.read_csv(url,skiprows=(8),skipfooter=9,engine='python')
-                pomme['annee'] = pomme['time'].str[:4]
-
-                pomme = pomme.groupby(['annee'])['G(i)', 'WS10m'].mean()
-                pomme['latitude'] = latitude
-                pomme["longitude"] = longitude
-
-                ls.append(pomme)
                 
-                #print("POMME", pomme)
+            except urllib.error.HTTPError:     #au cas où l'on tombe sur des lieux sans mesures, ignorer
+                pass    
                 
-            except urllib.error.HTTPError:     
-                pomme =[np.nan,np.nan,np.nan,np.nan]
             
-
-            #print(temp)
-    df_final = pd.concat(ls, axis=0, ignore_index=False)
-            
+    df_final = pd.concat(ls_df, axis=0, ignore_index=False) #Concaténation des différents df (un par coordonnées)
+    df_final.reset_index(level=0, inplace=True) #On enleve l'indexation par année
+    
+    
+    
+    
+    df_final.to_csv('BaseDonneesEnsoleilementPythonDS.csv')  #Mettre le chemin d'accès et le nom du fichier avant le .csv    
     return df_final
 
-df = zone(40,50,3,5,2,2013,2016)
-df.reset_index(level=0, inplace=True)
-df["size"] = pd.Series([50 for x in range(len(df.index))])
-import plotly.express as px
 
-# fig = px.density_mapbox(df, lat='latitude', lon='longitude', z='G(i)', radius=30,
-#                         center=dict(lat=0, lon=180), zoom=0,
-#                         mapbox_style="carto-positron")
-# fig.show()
-fig = px.scatter_mapbox(df.dropna(), lat="latitude", lon="longitude", color="G(i)", 
-                        size ="size",
-                        mapbox_style="carto-positron",color_continuous_scale=[[0, 'blue'], [0.15, 'green'],[0.30, "yellow"], [0.45, 'orange'], [0.60, 'pink'], [0.75, 'crimson'] ,[1.0, 'red']],
+
+###-----------------Création d'une carte intéractive-----------------------
+
+def carte_interactive(df,parametre):
+    """Création d'une carte intéractive avec plotly, permet de visualiser les données
+        Paramètre = "G(i)" [ensoleillement] ou "WS10m" [vitesse du vent]
+        Il est nécessaire d'avoir construit le df d'abord au moyen de la fonction creation_df_zone
+        
+        Exemple : carte_interactive(df, "G(i)")
+        """
+    df["size"] = pd.Series([50 for x in range(len(df.index))])  #Création d'une variable de taille pour les 
+    
+    fig = px.scatter_mapbox(df.dropna(), #On se débarasse des valeurs manquantes
+                        lat="latitude", #On donne les variables de coordonnées 
+                        lon="longitude", 
+                        color=str(parametre), #La couleur vient du parametre regardé : "G(i)" ou "WS10m"
+                        size ="size", #La taille de chaque point
+                        mapbox_style="carto-positron", #Le fond de carte
+                        
+                        #On joue avec les paramètres de légende pour les rendre cohérent avec les paramètres étudiés et consistants à travers le temps
+                        color_continuous_scale=[[0, 'blue'], [0.15, 'green'],[0.30, "yellow"], [0.45, 'orange'], [0.60, 'pink'], [0.75, 'crimson'] ,[1.0, 'red']],
+                        color_continuous_midpoint = df[parametre].mean(),
+                        range_color=([df[parametre].min(),df[parametre].max()]),
+                        
+                        #On anime le tout en fonction de l'année
                         animation_frame = 'annee')
-fig.show()
+    
+    fig.show()
 
-# fig = go.Figure(go.Scattermapbox(
-#     fill = "toself",
-#     lon = df["longitude"] , lat = df["latitude"],
-#     marker = { 'size': 30, 'color': df["G(i)"] }))
-# fig.show()
 
+
+###--------Exécution du code--------------------------
+
+df = creation_df_zone(40,60,-2,8,2,2013,2016)
+carte_interactive(df,"G(i)")
 
 
